be7d8c7b807a290c8146d67762c9b88d5251f183
chaos <chaos192837@gmail.com>
2017-03-22 18:57:07 +0800

    Levelize
    Complete EDAUtils::orderByLevel()

--- src/EDAUtils/EDAUtils.h
+++ src/EDAUtils/EDAUtils.h
@@ -7,9 +7,43 @@
 class EDAUtils
 {
     public:
+        /**
+         * Levelize the all gate/cell in Circuit(sequential/combinational),
+         * consider using orderCellByLevel() or orderGateByLevel()
+         * @param Circuit & : circuit object
+         *
+         * Example:
+         *  Circuit c17("c17.v");
+         *  EDAUtils::levelize(c17);
+         */
         static void levelize(const Circuit &);
-        static void orderGateByLevel(const Circuit &, std::vector<Gate> &);
-        static void orderCellByLevel(const Circuit &, std::vector<Cell> &);
+
+        /**
+         * Levelize Circuit(sequential/combinational) and feedback the levelized Gates
+         * @param Circuit & : circuit object
+         * @param std::vector<Gate> & : an Gate container
+         *
+         * Example:
+         *  Circuit c17("c17.v");
+         *  std::vector<Gate> gateList;
+         *  EDAUtils::orderGateByLevel(c17, gateList);
+         */
+        static void orderByLevel(const Circuit &, std::vector<Gate> &);
+
+        /**
+         * Levelize Circuit(sequential/combinational) and feedback the levelized Cell
+         * @param Circuit & : circuit object
+         * @param std::vector<Cell> & : an Cell container
+         *
+         * Example:
+         *  Circuit c17("c17.v");
+         *  std::vector<Cell> cellList;
+         *  EDAUtils::orderCellByLevel(c17, cellList);
+         */
+        static void orderByLevel(const Circuit &, std::vector<Cell> &);
+
+        static void removeDFF(Circuit &);
+        static void timeFrameExpansion(Circuit &, unsigned);
 };
 
 #endif
